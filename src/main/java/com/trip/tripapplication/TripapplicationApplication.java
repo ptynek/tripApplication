d0cd128573e11e121bdcaf00b3bf82b28ace4c5d
Piotr Tynek <tynek.piotr@gmail.com>
2022-10-13 19:18:08 +0200

    First commit

---
+++ src/main/java/com/trip/tripapplication/TripapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.trip.tripapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripapplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trip/tripapplication/TripapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.tripapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
